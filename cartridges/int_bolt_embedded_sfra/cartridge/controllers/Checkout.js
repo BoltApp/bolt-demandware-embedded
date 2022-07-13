22360c2fbd8c5d3ba2f9bd312d5b9e56fc22fbaf
DerrickGHW <derrick@bolt.com>
2022-07-13 15:19:58 -0400

    refactor

--- cartridges/int_bolt_embedded_sfra/cartridge/controllers/Checkout.js
+++ cartridges/int_bolt_embedded_sfra/cartridge/controllers/Checkout.js
@@ -7,12 +7,12 @@ server.extend(page);
 
 /* Script Modules */
 var BoltPreferences = require('~/cartridge/scripts/util/preferences');
-var constants = require('~/cartridge/scripts/util/constants');
+var boltAccountUtils = require('~/cartridge/scripts/util/boltAccountUtils');
 
 server.append('Begin', function (req, res, next) {
     var configuration = BoltPreferences.getSitePreferences();
     var basket = BasketMgr.getCurrentBasket();
-    var boltStoredPaymentMethods = session.privacy.boltOauthToken ? JSON.parse(basket.custom.boltPaymentMethods) : null;
+    var boltStoredPaymentMethods = boltAccountUtils.loginAsBoltUser() ? JSON.parse(basket.custom.boltPaymentMethods) : null;
     res.render('checkout/checkout', {
         config: configuration,
         boltStoredPaymentMethods: boltStoredPaymentMethods,
