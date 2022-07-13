9f21121ce367cde8056a0e492e29052df7527efa
DerrickGHW <derrick@bolt.com>
2022-07-13 15:09:18 -0400

    cleanup

--- cartridges/int_bolt_embedded_sfra/cartridge/client/default/js/boltStoredPayments.js
+++ cartridges/int_bolt_embedded_sfra/cartridge/client/default/js/boltStoredPayments.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
     const radioButtonLoaded = setInterval(function (){
-        console.log("loading radio buttons ");
         const useExistingCardBtn = $('#use-existing-card-radio-button');
         const addNewCardBtn = $('#add-a-new-card-radio-button');
         if (useExistingCardBtn && addNewCardBtn){
-            console.log("radio button loaded");
             clearInterval(radioButtonLoaded);
             useExistingCardBtn.click(function () {
                 if(this.checked){
