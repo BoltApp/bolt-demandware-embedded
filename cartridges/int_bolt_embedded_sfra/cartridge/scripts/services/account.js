6bb6b01628566c8e6d599675ec1df336cec469af
Sepandar <sepandar@bolt.com>
2022-07-14 08:27:25 -0600

    Removed unnecessary field

--- cartridges/int_bolt_embedded_sfra/cartridge/scripts/services/account.js
+++ cartridges/int_bolt_embedded_sfra/cartridge/scripts/services/account.js
@@ -122,8 +122,6 @@ function addPaymentMethodInfoToBasket(basket, boltPaymentMethods){
         );
 
         paymentInstrument.custom.basketId = basket.UUID;
-        paymentInstrument.custom.boltCardLastDigits =
-            boltPaymentMethod.last4;
         // TODO: don't have this info at this point, do we need to add it later
         //paymentInstrument.custom.boltCardBin = paymentInformation.bin;
         paymentInstrument.custom.boltPaymentMethodId = boltPaymentMethod.id;
