a0a5dacdfdaa21070e38d4e7e8ece806651bf288
DerrickGHW <derrick@bolt.com>
2022-07-19 12:39:12 -0400

    exclude the name

--- .circleci/scripts/test-compile-error.sh
+++ .circleci/scripts/test-compile-error.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-ExpectedErrorCount=1
-COMPILESUCCESS=$(npm run compile | grep "Error" | wc -l | xargs)
+ZERO=0
+COMPILESUCCESS=$(npm run compile | grep -v "formErrors" | grep "Error" | wc -l | xargs)
 
-if [ "$COMPILESUCCESS" -eq "$ExpectedErrorCount" ]
+if [ "$COMPILESUCCESS" -eq "$ZERO" ]
 then
     echo "npm run compile ran without errors"
 else
-    echo "error count: " $COMPILESUCCESS
     echo "npm run compile had errors"
     exit 1
 fi
