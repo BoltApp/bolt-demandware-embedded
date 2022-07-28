103692b67d0575baebc185d43d1b6d5d37300d6e
DerrickGHW <derrick@bolt.com>
2022-07-28 12:32:07 -0400

    small update

--- cartridges/int_bolt_embedded_sfra/cartridge/controllers/CheckoutServices.js
+++ cartridges/int_bolt_embedded_sfra/cartridge/controllers/CheckoutServices.js
@@ -5,14 +5,18 @@ var server = require('server');
 
 // Script includes
 var boltAccountUtils = require('~/cartridge/scripts/util/boltAccountUtils');
+var logUtils = require('~/cartridge/scripts/util/boltLogUtils');
+var log = logUtils.getLogger('CheckoutService');
 
 server.extend(module.superModule);
 
 server.append('PlaceOrder', function (req, res, next) {
     if (!res.viewData.error) {
         // clear session data after order placed successfully
+        log.info('clearing session data');
         boltAccountUtils.clearBoltSessionData();
     }
+    res.json(res);
 
     next();
 });
