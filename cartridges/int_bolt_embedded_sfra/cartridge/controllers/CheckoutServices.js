a8f62ec53545032ee8c44d4bb80c273f1bd7bf7a
DerrickGHW <79222730+DerrickGHW@users.noreply.github.com>
2022-07-28 13:24:07 -0400

    clear session data if auth succeeds (#53)
    * clear session data if auth succeeds
    
    * clear session data upon a successful order
    
    * small update
    
    * remove newline
    
    * remove the line

---
+++ cartridges/int_bolt_embedded_sfra/cartridge/controllers/CheckoutServices.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* API Includes */
+var server = require('server');
+
+// Script includes
+var boltAccountUtils = require('~/cartridge/scripts/util/boltAccountUtils');
+var logUtils = require('~/cartridge/scripts/util/boltLogUtils');
+var log = logUtils.getLogger('CheckoutService');
+
+server.extend(module.superModule);
+
+server.append('PlaceOrder', function (req, res, next) {
+    if (!res.viewData.error) {
+        // clear session data after order placed successfully
+        log.info('clearing session data');
+        boltAccountUtils.clearBoltSessionData();
+    }
+
+    next();
+});
+
+module.exports = server.exports();
