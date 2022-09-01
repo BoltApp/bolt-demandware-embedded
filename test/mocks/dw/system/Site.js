6730aa13be0a01e2fd360dccbc292854c85f8551
DerrickGHW <derrick@bolt.com>
2022-09-01 10:09:05 -0400

    add tests for site preferences values

--- test/mocks/dw/system/Site.js
+++ test/mocks/dw/system/Site.js
@@ -1,7 +1,35 @@
-exports.getCurrent = function () {
+const boltMerchantDivisionID = 'boltMerchantDivisionID';
+const boltMultiPublishableKey = 'publishable_key';
+const boltEnable = true;
+const boltEnvironment = 'sandbox';
+const boltApiURL = 'https://api-sandbox.bolt.com';
+const boltConnectURL = 'https://connect-sandbox.bolt.com';
+
+function getCurrent() {
     return {
-        getCustomPreferenceValue: function () {
-            return 'SOME_VALUE';
+        getCustomPreferenceValue: function (val) {
+            switch (val) {
+                case 'boltMultiPublishableKey':
+                    return boltMultiPublishableKey;
+                case 'boltEnable':
+                    return boltEnable;
+                case 'boltMerchantDivisionID':
+                    return boltMerchantDivisionID;
+                case 'boltEnvironment':
+                    return boltEnvironment;
+                default:
+                    return 'some_custom_preference_value';
+            }
         }
     };
 }
+
+module.exports = {
+    boltMerchantDivisionID: boltMerchantDivisionID,
+    boltMultiPublishableKey: boltMultiPublishableKey,
+    boltEnable: boltEnable,
+    boltEnvironment: boltEnvironment,
+    boltApiURL: boltApiURL,
+    boltConnectURL: boltConnectURL,
+    getCurrent: getCurrent
+};
--- test/unit/int_bolt_embedded_sfra/script/util/preference.js
+++ test/unit/int_bolt_embedded_sfra/script/util/preference.js
@@ -6,6 +6,7 @@ var assert = require('chai').assert;
 var expect = chai.expect;
 chai.use(sinonChai);
 var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
+var site = require('../../../../mocks/dw/system/Site');
 
 describe('preference', function () {
     var preference = proxyquire('../../../../../cartridges/int_bolt_embedded_sfra/cartridge/scripts/util/preferences', {
@@ -16,16 +17,15 @@ describe('preference', function () {
     it('getSitePreferences returns all site preferences', function () {
         var response = preference.getSitePreferences();
 
-        expect(response.boltEnable).to.not.be.null;
-        expect(response.boltMerchantDivisionID).to.not.be.null;
-        expect(response.boltApiUrl).to.not.be.null;
-        expect(response.boltCdnUrl).to.not.be.null;
-        expect(response.boltMultiPublishableKey).to.not.be.null;
+        expect(response.boltEnable).to.be.equal(site.boltEnable);
+        expect(response.boltMerchantDivisionID).to.be.equal(site.boltMerchantDivisionID);
+        expect(response.boltApiUrl).to.be.equal(site.boltApiURL);
+        expect(response.boltCdnUrl).to.be.equal(site.boltConnectURL);
+        expect(response.boltMultiPublishableKey).to.be.equal(site.boltMultiPublishableKey);
     });
 
     it('getBoltAPIKey returns the api key', function () {
         var response = preference.getBoltAPIKey();
         expect(response).to.not.be.null;
     });
-
 });
