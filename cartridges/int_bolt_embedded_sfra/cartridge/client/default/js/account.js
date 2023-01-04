ffa0d2cd4d868e557368d2a6357935f075ef96da
justinBolt1987 <jxie@bolt.com>
2023-01-04 16:19:30 -0500

    Fix auto login issue in slow network speed.

--- cartridges/int_bolt_embedded_sfra/cartridge/client/default/js/account.js
+++ cartridges/int_bolt_embedded_sfra/cartridge/client/default/js/account.js
@@ -199,3 +199,24 @@ exports.displayCustomerInfo = function () {
     $('#default-customer-status').show();
     $('#login-status').hide();
 };
+
+/**
+ * Get cookie from browser
+ * @param {string} cookieName - cookie name
+ * @returns {string} cookie value
+ */
+exports.getCookie = function (cookieName) {
+    let name = cookieName + '=';
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) { // eslint-disable-line no-plusplus
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return '';
+};
