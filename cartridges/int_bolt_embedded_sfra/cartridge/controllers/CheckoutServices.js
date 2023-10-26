7f1f4bf7085d11f51551a3b18c4424604a992d03
Serena Yan <91094713+serenayanbolt@users.noreply.github.com>
2023-10-26 17:51:44 -0400

    clear session data

--- cartridges/int_bolt_embedded_sfra/cartridge/controllers/CheckoutServices.js
+++ cartridges/int_bolt_embedded_sfra/cartridge/controllers/CheckoutServices.js
@@ -2,16 +2,26 @@
 
 /* API Includes */
 var server = require('server');
+var Site = require('dw/system/Site');
+// Script includes
+var boltAccountUtils = require('~/cartridge/scripts/util/boltAccountUtils');
+var logUtils = require('~/cartridge/scripts/util/boltLogUtils');
+var log = logUtils.getLogger('CheckoutService');
 
 server.extend(module.superModule);
 
 server.append('PlaceOrder', function (req, res, next) {
-    if (!res.viewData.error) {
-        // reset boltRedirectCheckout in the session, so Bolt can fill the checkout form automatically from shopper account data
-        session.privacy.boltRedirectCheckout = true;
+  if (!res.viewData.error) {
+    if (Site.getCurrent().getCustomPreferenceValue('boltEnableSSO')) {
+      // reset boltRedirectCheckout in the session, so Bolt can fill the checkout form automatically from shopper account data
+      session.privacy.boltRedirectCheckout = true;
+    } else {
+      // clear session data after order placed successfully
+      log.info('clearing session data after a successful order!');
+      boltAccountUtils.clearBoltSessionData();
     }
-
-    next();
+  }
+  next();
 });
 
 module.exports = server.exports();
