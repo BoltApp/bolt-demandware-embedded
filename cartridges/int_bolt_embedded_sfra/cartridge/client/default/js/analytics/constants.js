2f8aa9482dac78d621f6825a791c878ea16bcd16
Alan Thai <athai@bolt.com>
2024-04-12 10:55:48 -0400

    Analytics rename

--- cartridges/int_bolt_embedded_sfra/cartridge/client/default/js/analytics/constants.js
+++ cartridges/int_bolt_embedded_sfra/cartridge/client/default/js/analytics/constants.js
@@ -1,25 +1,25 @@
 'use strict';
 
 exports.EventName = {
-    CHECKOUT_LOADED: 'Checkout | loaded',
+    CHECKOUT_LOADED: 'checkout . loaded',
 
-    RECOGNITION_EMAIL_FIELD_CHANGED: 'Recognition | email field changed',
+    RECOGNITION_EMAIL_FIELD_CHANGED: 'recognition . email field changed',
 
-    SHIPPING_STEP_ENTERED: 'Shipping | step entered',
-    SHIPPING_DETAILS_FULLY_ENTERED: 'Shipping | details fully entered',
-    SHIPPING_OPTION_SELECTED: 'Shipping | option selected',
-    SHIPPING_SUBMITTED: 'Shipping | submitted',
-    SHIPPING_EDIT_BUTTON_CLICKED: 'Shipping | edit button clicked',
+    SHIPPING_STEP_ENTERED: 'shipping . step entered',
+    SHIPPING_DETAILS_FULLY_ENTERED: 'shipping . details fully entered',
+    SHIPPING_OPTION_SELECTED: 'shipping . option selected',
+    SHIPPING_SUBMITTED: 'shipping . submitted',
+    SHIPPING_EDIT_BUTTON_CLICKED: 'shipping . edit button clicked',
 
-    BILLING_STEP_ENTERED: 'Billing | step entered',
-    BILLING_ADDRESS_DETAILS_ENTERED: 'Billing | address details entered',
-    BILLING_PAYMENT_METHOD_SELECTED: 'Billing | payment method selected',
-    BILLING_SUBMITTED: 'Billing | submitted',
-    BILLING_EDIT_BUTTON_CLICKED: 'Billing | edit button clicked',
+    BILLING_STEP_ENTERED: 'billing . step entered',
+    BILLING_ADDRESS_DETAILS_ENTERED: 'billing . address details entered',
+    BILLING_PAYMENT_METHOD_SELECTED: 'billing . payment method selected',
+    BILLING_SUBMITTED: 'billing . submitted',
+    BILLING_EDIT_BUTTON_CLICKED: 'billing . edit button clicked',
 
-    PAYMENT_STEP_ENTERED: 'Payment | step entered',
-    PAYMENT_PAY_BUTTON_CLICKED: 'Payment | pay button clicked',
-    PAYMENT_SUCCEEDED: 'Payment | succeeded',
-    PAYMENT_FAILED: 'Payment | failed',
-    PAYMENT_TOGGLE_REGISTRATION_CHECKBOX: 'Payment | toggle registration checkbox'
+    PAYMENT_STEP_ENTERED: 'payment . step entered',
+    PAYMENT_PAY_BUTTON_CLICKED: 'payment . pay button clicked',
+    PAYMENT_SUCCEEDED: 'payment . succeeded',
+    PAYMENT_FAILED: 'payment . failed',
+    PAYMENT_TOGGLE_REGISTRATION_CHECKBOX: 'payment . toggle registration checkbox'
 };
